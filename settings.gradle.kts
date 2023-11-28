23bb9c49f5e8df4c62a8e74fc725c25b5d0be457
Henri <henri.schatz@gmx.de>
2023-11-28 09:14:05 +0100

    project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Material Rental Backend"
---
+++ src/main/kotlin/com/rental/plugins/Routing.kt
@@ -0,0 +1,30 @@
+package com.rental.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.autohead.*
+import io.ktor.server.plugins.doublereceive.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    install(DoubleReceive)
+    install(AutoHeadResponse)
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        post("/double-receive") {
+            val first = call.receiveText()
+            val theSame = call.receiveText()
+            call.respondText(first + " " + theSame)
+        }
+    }
+}
